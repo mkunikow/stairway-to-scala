d7d29eb6d4aa102e29118050bbecbad695bea490
mkunikowski <michal.kunikowski@gmail.com>
2015-02-17 23:52:58 +0100

    Flight07 completed

--- Flight07/src/test/scala/koans/Flight07.scala
+++ Flight07/src/test/scala/koans/Flight07.scala
@@ -14,16 +14,32 @@ class Flight07 extends KoanSuite with Matchers with SeveredStackTraces {
   // a class heirarchy that satisfies the tests below when they are uncommented
 
   // abstract class RollingStock should have an abstract field, name, of type String
+  abstract class RollingStock {
+    val name: String
+  }
 
   // abstract class Car should subclass RollingStock and add a field carries of type String
   // and a method (with implementation) called pulled that returns a string formed of
   // name + " carrying " + carries
+  abstract class Car extends RollingStock {
+    val carries: String
+    def pulled: String = name + " carrying " + carries
+    override def toString = pulled
+  }
 
   // concrete class PassengerCar should subclass Car, set the name field to be "Passenger car" and
   // the carries field to "people"
+  class PassengerCar extends Car {
+    override val carries: String = "people"
+    override val name: String = "Passenger car"
+  }
 
   // concrete class CargoCar should subclass Car, set the name field "Cargo car" and
   // the carries field to "cargo"
+  class CargoCar extends Car {
+    override val carries: String = "cargo"
+    override val name: String = "Cargo car"
+  }
 
   // now create an abstract Engine class that subclasses RollingStock and has the following:
   // a cars field with a mutable.ListBuffer of Cars
@@ -40,15 +56,30 @@ class Flight07 extends KoanSuite with Matchers with SeveredStackTraces {
   // a toString on each object, which doesn't return the string you want, either you will
   // need to define toString for the cars with some meaningful implementation (maybe forward 
   // to pulled) or else you will need to convert them all to the strings you want first.
+  abstract class Engine extends RollingStock {
+    val cars: mutable.ListBuffer[Car] = new mutable.ListBuffer()
+    val pullActivity = "pulls"
+    def pull:String = name + " " + pullActivity +" " + cars.mkString(" and ")
+    val maxCars: Int
+    def add(car: Car) = if (cars.size < maxCars) {cars += car; this} else throw new IllegalStateException()
+  }
 
   // Create a concrete class SteamEngine that subclasses Engine, sets the name field to "Steam engine"
   // and the maxCars field to 3
+  class SteamEngine extends Engine {
+    override val maxCars: Int = 3
+    override val name: String = "Steam engine"
+  }
 
   // Create a concrete class DieselEngine that subclasses Engine, sets the name field to "Diesel engine"
   // and the maxCars field to 6
+  class DieselEngine extends Engine {
+    override val maxCars: Int = 6
+    override val name: String = "Diesel engine"
+  }
 
   // now uncomment the following tests (first comment block only) and run them to make sure they pass
-  /* test ("2 passenger car steam train") {
+   test ("2 passenger car steam train") {
     val steamEngine = new SteamEngine
     steamEngine.add(new PassengerCar)
     steamEngine.add(new PassengerCar)
@@ -83,14 +114,19 @@ class Flight07 extends KoanSuite with Matchers with SeveredStackTraces {
                                  "Cargo car carrying cargo and " +
                                  "Passenger car carrying people and " +
                                  "Cargo car carrying cargo")
-  } */
+  }
 
   // now create a new class - ShuntEngine that overrides the pull method to print the string:
   // name + " doesn't pull, it pushes " and then the rest of the train description as before. It should
   // also provide the name as "Shunt engine" and the maxCars as 10 (shunt engines are strong :-) )
   // then uncomment the test below and make sure it passes
+  class ShuntEngine extends Engine{
+    override val maxCars: Int = 10
+    override val pullActivity = "doesn't pull, it pushes"
+    override val name: String = "Shunt engine"
+  }
 
-  /* test ("Shunt engine with 8 cars") {
+   test ("Shunt engine with 8 cars") {
     val shuntEngine = new ShuntEngine
     shuntEngine.add(new CargoCar)
     shuntEngine.add(new PassengerCar)
@@ -109,7 +145,7 @@ class Flight07 extends KoanSuite with Matchers with SeveredStackTraces {
                                  "Passenger car carrying people and " +
                                  "Cargo car carrying cargo and " +
                                  "Passenger car carrying people")  
-  } */
+  }
 
   // now, let's make the train creation a little more readable with some factory methods. Create
   // an object called Engine with factory methods to create the different engine types (call the methods
@@ -117,12 +153,18 @@ class Flight07 extends KoanSuite with Matchers with SeveredStackTraces {
   // the test below and make sure it passes.
 
   object Engine {
+    def diesel = new DieselEngine()
+    def steam = new SteamEngine()
+    def shunt = new ShuntEngine()
+
   }
   
   object Car {
+    def passenger = new PassengerCar()
+    def cargo = new CargoCar()
   }
 
-  /* test ("Train factory") {
+   test ("Train factory") {
     val steamEngine = Engine.steam
     steamEngine add Car.passenger    // crafty use of infix operator syntax to make it read nicely
     steamEngine add Car.passenger
@@ -160,24 +202,24 @@ class Flight07 extends KoanSuite with Matchers with SeveredStackTraces {
                                  "Passenger car carrying people and " +
                                  "Cargo car carrying cargo and " +
                                  "Passenger car carrying people")
-  } */
+  }
 
   // extra credit - alter the add method in the Engine abstract class to return the engine instance
   // at the end of the method, so that the cars can be added in a chain like this:
   //
-  // val steamTrain = Engine.steam add Car.cargo add Car.passenger add Car.cargo
+  val steamTrain = Engine.steam add Car.cargo add Car.passenger add Car.cargo
   //
   // and write a test to make sure that it works, as expected, for all trains and carriages
   //
   // Why didn't that change break your existing tests?
   // You have effectively created a simple DSL for creating trains, how do you like that?
 
-  /* test ("Chained train creation") {
+   test ("Chained train creation") {
     val steamEngine = Engine.steam add Car.cargo add Car.passenger add Car.passenger
 
     steamEngine.pull should be ("Steam engine pulls Cargo car carrying cargo and Passenger car carrying people and Passenger car carrying people")
     
-  } */
+  }
 
   // extra extra credit: there may be some repetition in your ShuntEngine implementation where it lists
   // out the cars in the overridden pull method. Refactor out the car string resolution into a separate
